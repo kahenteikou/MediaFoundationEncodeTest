90a0b017bafc0c3a52161dd92a350ca99fa9a8a6
kahenteikou <kokkiemouse@gmail.com>
2022-06-16 19:56:22 +0900

    a2

--- MediaFoundationEncodeTest/main.cpp
+++ MediaFoundationEncodeTest/main.cpp
@@ -1,4 +1,34 @@
 #include "stdafx.h"
-int main() {
-	return 0;
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+	switch (msg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProcW(hwnd, msg, wp, lp);
+}
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	HWND hwnd;
+	MSG msg;
+	WNDCLASS wc;
+
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = TEXT("MainWindow_MFApp");
+	if (!RegisterClass(&wc)) return -1;
+	hwnd = CreateWindow(TEXT("MainWindow_MFApp"), TEXT("Title Bar?"),
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, NULL, hInstance, NULL);
+	if (hwnd == NULL) return -1;
+	while (GetMessage(&msg, NULL, 0, 0))DispatchMessage(&msg);
+	return msg.wParam;
 }
